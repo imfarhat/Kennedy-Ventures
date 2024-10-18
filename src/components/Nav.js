e988bf440e2c94b6981bf0e8d2a333a5a016eedb
I Farhat <ifarhat.dev@gmail.com>
2024-10-18 19:16:31 +0530

    add animated gift for nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import Logo from "@/assets/circular-logo.png";
 import Link from "next/link";
+import LogoGif from "@/assets/Kennedy-venture.gif";
 
 const Nav = () => {
   return (
     <header className="bg-primary text-secondary z-40 flex items-center justify-center sticky top-0 shadow-md">
       <nav className="container pr-4 pl-0.5 w-full flex items-center justify-between">
-        <Link href="/" className="flex items-center justify-center flex-row">
+        <Link href="/" className="max-h-20">
+          <Image
+            src={LogoGif}
+            height={146.25}
+            width={260}
+            priority
+            className="-mx-6 -my-6 object-cover"
+            alt="Kennedy Ventures Logo"
+            unoptimized={true}
+          />
+        </Link>
+        {/* <Link href="/" className="flex items-center justify-center flex-row">
           <Image
             src={Logo}
             height={80}
@@ -24,7 +36,7 @@ const Nav = () => {
               Ventures
             </p>
           </div>
-        </Link>
+        </Link> */}
         <div className="py-4 md:py-6 flex items-center justify-center gap-4 font-bold">
           <Link href="/">Home</Link>
           <Link href="/portfolio">Portfolio</Link>
