cb2e269b23bb0b628071d1e1e8f4d12b6842ed3a
I Farhat <ifarhat.dev@gmail.com>
2024-10-18 16:31:36 +0530

    auto update current year copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="border-t px-4 flex items-center justify-center">
       <div className="container px-4 py-4">
         <em className="text-xs md:text-sm font-medium">
-          &copy; 2024 Copyright Kennedy Ventures, B.V.
+          &copy; {new Date().getFullYear()} Copyright Kennedy Ventures, B.V.
         </em>
       </div>
     </footer>
