1fbdb96018640d3e4e3de2288e8e5307d1d3502b
I Farhat <ifarhat.dev@gmail.com>
2024-10-18 10:23:05 +0530

    adjust alignment

--- src/app/page.js
+++ src/app/page.js
@@ -7,22 +7,22 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-start justify-center">
       <section
-        className="bg-cover flex flex-col items-center justify-start relative py-20 bg-secondary/50 w-full bg-center"
+        className="bg-cover flex flex-col items-center justify-start relative py-12 md:py-16 bg-secondary/50 w-full bg-center"
         style={{
           backgroundImage: `url(${RunnderBgPng.src})`,
         }}
       >
         <div className="absolute inset-0 bg-black/65" />
         <div className="z-10">
-          <strong className="uppercase italic text-white">
+          <strong className="uppercase italic text-white text-sm md:text-base">
             *** Not Accepting New Clients ***
           </strong>
         </div>
         <div className="container px-4 flex flex-col items-start justify-center z-10">
-          <h2 className="mt-20 text-8xl font-extrabold text-primary max-w-[50rem]">
+          <h2 className="mt-6 md:mt-8 text-5xl md:text-8xl font-extrabold text-primary max-w-[50rem]">
             Are you ready to RUN
           </h2>
-          <p className="text-white font-medium mt-20 max-w-[60rem]">
+          <p className="text-white font-medium mt-8 md:mt-16 max-w-[60rem] text-sm md:text-base">
             Kennedy Ventures supports promising startups with the tools, the
             tech, and the capital to build and grow to their peak potential. If
             you&apos;re ready to move your idea forward and level up your
@@ -30,16 +30,16 @@ export default function Home() {
             bozos in this stock photo, be sure to bring the appropriate
             footwear.
           </p>
-          <div className="flex flex-wrap items-center justify-start gap-4 md:gap-6 mt-20 w-full">
+          <div className="grid grid-cols-2 items-center justify-start gap-4 md:gap-6 mt-12 md:mt-16 w-full max-w-96 md:max-w-[30rem]">
             <Button
-              className="text-secondary font-semibold rounded-none px-10 py-6"
+              className="text-secondary font-semibold rounded-none py-6"
               asChild
             >
               <Link href="/appointment">Book Appointment</Link>
             </Button>
             <Button
               variant="secondary"
-              className="text-white rounded-none px-10 py-6"
+              className="text-white rounded-none py-6"
               asChild
             >
               <Link href="/portfolio">See Portfolio</Link>
