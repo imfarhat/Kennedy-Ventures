653f84429a3c32c3699198f4e7fa89566162c491
I Farhat <ifarhat.dev@gmail.com>
2024-10-18 10:04:02 +0530

    add seo medatadat for open graph social media share

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,11 @@ export const metadata = {
   title: "Kennedy Ventures",
   description:
     "Kennedy Ventures supports promising startups with the tools, the tech, and the capital to build and grow to their peak potential. If you're ready to move your idea forward and level up your business, set an appointment using the link below. And unlike the bozos in this stock photo, be sure to bring the appropriate footwear.",
+  openGraph: {
+    title: "Kennedy Ventures",
+    description:
+      "Kennedy Ventures supports promising startups with the tools, the tech, and the capital to build and grow to their peak potential. If you're ready to move your idea forward and level up your business, set an appointment using the link below. And unlike the bozos in this stock photo, be sure to bring the appropriate footwear.",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -27,10 +32,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Nav />
-  
-          {children}
-          {/* <Loading /> */}
-       
+
+        {children}
+        {/* <Loading /> */}
       </body>
     </html>
   );
