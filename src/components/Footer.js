587ff835d242f17794fdc75afd01296e6c3ce05c
I Farhat <ifarhat.dev@gmail.com>
2024-10-18 12:08:25 +0530

    fix alignment issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="container px-4 flex items-center justify-start">
-      <em className="text-xs md:text-sm font-medium py-4">
-        &copy; 2024 Copyright Kennedy Ventures, B.V.
-      </em>
+    <footer className=" px-4 flex items-center justify-center">
+      <div className="container px-4 py-4">
+        <em className="text-xs md:text-sm font-medium">
+          &copy; 2024 Copyright Kennedy Ventures, B.V.
+        </em>
+      </div>
     </footer>
   );
 };
