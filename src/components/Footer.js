a5301ebd1fdc06879d1839715885e41d70f9ada2
I Farhat <ifarhat.dev@gmail.com>
2024-10-18 12:05:54 +0530

    fix typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="container px-4 flex items-center justify-start">
       <em className="text-xs md:text-sm font-medium py-4">
-        &copy; 2024 Copyright Kennedey Ventures, B.V.
+        &copy; 2024 Copyright Kennedy Ventures, B.V.
       </em>
     </footer>
   );
