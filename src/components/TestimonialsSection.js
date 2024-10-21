2b87555a66675f2b2421ce451471c56552be89fa
I Farhat <ifarhat.dev@gmail.com>
2024-10-21 19:21:57 +0530

    add CTA before testimonials

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -117,7 +117,7 @@ const TestimonialsSection = () => {
             delay: 2500,
           }),
         ]}
-        className="container px-4"
+        className="container px-4 flex items-center justify-center"
       >
         <CarouselContent className="mt-8 flex space-x-4">
           {testimonialsData.map((testimonial, index) => (
@@ -132,6 +132,11 @@ const TestimonialsSection = () => {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+      <div className="container px-4">
+        <h3 className="mt-8 text-secondary font-semibold text-lg sm:text-xl md:text-2xl w-full text-center">
+          Your startup can be here. Contact us!
+        </h3>
+      </div>
     </section>
   );
 };
