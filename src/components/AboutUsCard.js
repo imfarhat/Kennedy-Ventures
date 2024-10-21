163644cccd938955d2ef1d3dca38acc5725ebb11
I Farhat <ifarhat.dev@gmail.com>
2024-10-21 21:28:49 +0530

    add hover effect in about us card

--- src/components/AboutUsCard.js
+++ src/components/AboutUsCard.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 const AboutUsCard = ({ data }) => {
   return (
     <>
-      <aside className="flex flex-col items-center justify-center">
+      <aside className="flex flex-col items-center justify-center group">
         <Image
           src={`${data.imagePath}`}
           height={208}
           width={208}
           priority
-          className="size-52 bg-primary rounded-tr-[6.5rem] rounded-bl-[6.5rem] aspect-square"
+          className="size-52 bg-primary rounded-tr-[6.5rem] rounded-bl-[6.5rem] group-hover:rounded-tr-none group-hover:rounded-tl-[6.5rem] group-hover:rounded-bl-none group-hover:rounded-br-[6.5rem] transition-all ease-in aspect-square"
           alt="Scott Kennedy"
         />
         <span className="border-b-4 border-double border-secondary w-1/2 mt-4 md:mt-6"></span>
