98eb5fc4501cc8e3ae033c7f692be77f2c91ff99
I Farhat <ifarhat.dev@gmail.com>
2024-10-21 17:28:33 +0530

    enhnace testimonial card

--- src/app/page.js
+++ src/app/page.js
@@ -23,63 +23,63 @@ import { Quote } from "lucide-react";
 
 const testimonialsData = [
   {
-    imageFileName: "1.png",
+    imagePath: "/testimonials/1.png",
     title: "WeTime",
     description: "HR SaaS Tool for Teams",
     content:
       "We knew Kennedy Ventures would be a great fit from the initial consultation. In just a few months, we went from a simple idea to a complete 'investment ready' pitch.",
   },
   {
-    imageFileName: "2.png",
+    imagePath: "/testimonials/2.png",
     title: "Mutant",
     description: "Analytics for Developers",
     content:
       "Most consulting firms are all talk. But KV rolled up their sleeves and started working the very first week, asking the right questions and helping develop the core value proposition.",
   },
   {
-    imageFileName: "3.png",
+    imagePath: "/testimonials/3.png",
     title: "Yabbu",
     description: "SaaS Meeting Management",
     content:
       "We needed more than just an investment; we needed someone who could help develop a complete outbound sales funnel, then produce materials to engage prospects.",
   },
   {
-    imageFileName: "4.png",
+    imagePath: "/testimonials/4.png",
     title: "Boiler",
     description: "Collaborative Ideation Platform",
     content:
       "Kennedy Ventures offered us a remarkably affordable proposal, including access to their extensive partnership network. We highly recommend them.",
   },
   {
-    imageFileName: "5.png",
+    imagePath: "/testimonials/5.png",
     title: "Solidarity",
     description: "Organizing Tool for Unions",
     content:
       "In just three innovation sessions with Kennedy, we refined the mission, developed a financing strategy, and revamped the pitch deck. We'll keep using KV.",
   },
   {
-    imageFileName: "6.png",
+    imagePath: "/testimonials/6.png",
     title: "Aarini Consulting",
     description: "Leaders in Digital Architecture",
     content:
       "Kennedy led the innovation team to develop and expand Aarini into new markets, and helped us acquire new business partnerships. We are eager to work with them again.",
   },
   {
-    imageFileName: "7.png",
+    imagePath: "/testimonials/7.png",
     title: "Sonikize",
     description: "Digital Audio Enhancement",
     content:
       "They showed us a way to finance our growth without sacrificing our equity.",
   },
   {
-    imageFileName: "8.png",
+    imagePath: "/testimonials/8.png",
     title: "Mercearia",
     description: "Food Market and Catering Service",
     content:
       "We brought in KV to go over our business plan and pitch materials, and we're very happy with the results. We recommend them to any new organization eager to stand out in a crowded marketplace.",
   },
   {
-    imageFileName: "9.png",
+    imagePath: "/testimonials/9.png",
     title: "Wormhole",
     description: "Consumer Video Sharing App",
     content:
@@ -87,7 +87,7 @@ const testimonialsData = [
   },
 
   {
-    imageFileName: "10.png",
+    imagePath: "/testimonials/10.png",
     title: "GreenSwapp",
     description: "Climate Impact Analysis Platform",
     content:
@@ -95,7 +95,7 @@ const testimonialsData = [
   },
 
   {
-    imageFileName: "11.png",
+    imagePath: "/testimonials/11.png",
     title: "Barqo",
     description: "Boat Rental Platform",
     content:
@@ -352,8 +352,8 @@ export default function Home() {
             {testimonialsData.map((testimonial, index) => (
               <React.Fragment key={index}>
                 {index === testimonialsData.length - 1 && (
-                  <Card className="max-w-[350px] w-full h-full rounded-none relative">
-                    <CardHeader className="flex flex-row items-center justify-start gap-2 group-hover:bg-inherit backdrop-blur-sm transition ease-in">
+                  <Card className="max-w-[350px] w-full h-full group relative overflow-hidden hover:-translate-y-3 transition ease-in">
+                    <CardHeader className="flex flex-row items-center justify-start gap-2 group-hover:bg-secondary/5 transition ease-in">
                       <Image
                         src={"/placeholder.svg"}
                         height={80}
@@ -370,7 +370,7 @@ export default function Home() {
                         </CardDescription>{" "}
                       </div>
                     </CardHeader>
-                    <CardContent className="">
+                    <CardContent className="mt-4">
                       <Image
                         src={"/placeholder.svg"}
                         height={100}
@@ -379,7 +379,7 @@ export default function Home() {
                       />
                     </CardContent>
                     <CardFooter className="absolute bottom-0 right-0">
-                      <Quote className="text-muted-foreground/25 size-8" />
+                      <Quote className="text-secondary/5 drop-shadow-md size-8" />
                     </CardFooter>
                   </Card>
                 )}
--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -11,10 +11,10 @@ import { Quote } from "lucide-react";
 
 const TestimonialCard = ({ data }) => {
   return (
-    <Card className="max-w-[350px] w-full h-full rounded-none relative">
-      <CardHeader className="flex flex-row items-center justify-start gap-2">
+    <Card className="max-w-[350px] w-full h-full group relative overflow-hidden hover:-translate-y-3 transition ease-in">
+      <CardHeader className="flex flex-row items-center justify-start gap-2 group-hover:bg-secondary/5 transition ease-in">
         <Image
-          src={`/testimonials/${data.imageFileName}`}
+          src={`${data.imagePath}`}
           height={80}
           width={80}
           className="max-h-20 h-full object-cover"
@@ -25,7 +25,7 @@ const TestimonialCard = ({ data }) => {
           <CardDescription>{data.description}</CardDescription>
         </div>
       </CardHeader>
-      <CardContent>{data.content}</CardContent>
+      <CardContent className="mt-4">{data.content}</CardContent>
       <CardFooter className="absolute bottom-0 right-0">
         <Quote className="text-muted-foreground/25 size-8" />
       </CardFooter>
