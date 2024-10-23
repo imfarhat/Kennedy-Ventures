3d9b84e609575cb85291365b73e0b4c8132b1614
I Farhat <ifarhat.dev@gmail.com>
2024-10-23 18:36:35 +0530

    add robots file

---
+++ src/app/robots.js
@@ -0,0 +1,11 @@
+export default function robots() {
+  const baseUrl = process.env.BASE_URL;
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
