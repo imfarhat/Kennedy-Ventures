2f2c5dd3f479528949a39dbcb02439d3e901c3b2
I Farhat <ifarhat.dev@gmail.com>
2024-10-23 20:24:08 +0530

    add keywords and

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,13 +17,41 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Kennedy Ventures",
+  title: {
+    default: "Kennedy Ventures",
+    template: `%s | Kennedy Ventures`,
+  },
   description:
     "Kennedy Ventures supports promising startups with the tools, the tech, and the capital to build and grow to their peak potential. If you're ready to move your idea forward and level up your business, set an appointment using the link below. And unlike the bozos in this stock photo, be sure to bring the appropriate footwear.",
+
+  keywords: [
+    "Startup Support",
+    "Business Strategy Consulting",
+    "Innovative Product Development",
+    "Capital Investment for Startups",
+    "Marketing Strategy",
+    "Sales Optimization",
+    "Founder-Friendly Financing",
+    "Equity and Convertible Debt",
+    "Revenue Share Loans",
+    "Entrepreneurial Growth",
+    "Startup Innovation",
+    "Ecommerce Solutions",
+    "Fintech Consulting",
+    "Artificial Intelligence Startups",
+    "Business Model Strategy",
+    "Appointment Booking for Startups",
+    "Strategic Partnerships",
+    "Accelerate Startup Growth",
+    "Investment Opportunities",
+    "Tech Startup Consultancy",
+  ],
+  metadataBase: new URL(process.env.BASE_URL),
   openGraph: {
     title: "Kennedy Ventures",
     description:
       "Kennedy Ventures supports promising startups with the tools, the tech, and the capital to build and grow to their peak potential. If you're ready to move your idea forward and level up your business, set an appointment using the link below. And unlike the bozos in this stock photo, be sure to bring the appropriate footwear.",
+    images: ["/icon.png"],
   },
 };
 
