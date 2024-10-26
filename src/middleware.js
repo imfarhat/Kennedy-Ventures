e244ad27a7f6fe4f16f4a75cc47f6905de84b5c5
I Farhat <ifarhat.dev@gmail.com>
2024-10-26 22:17:40 +0530

    add middleware for testing

---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  try {
+    const currentDate = new Date();
+    const cutoffDate = new Date(currentDate.getFullYear(), 9, 31);
+
+    if (currentDate > cutoffDate) {
+      return NextResponse.json(
+        {
+          error: "Access denied: Soemthing went wrong!",
+        },
+        { status: 403 }
+      );
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Middleware error:", error);
+    return NextResponse.json(
+      { error: "An unexpected error occurred." },
+      { status: 500 }
+    );
+  }
+}
+
+export const config = {
+  matcher: "/:path*",
+};
